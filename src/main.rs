fdecdf8e9b76b928e6277a9ab50a18f30c6240e4
zalt00 <41543767+zalt00@users.noreply.github.com>
2022-02-16 10:27:30 +0100

    Add memoization

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::{VecDeque, HashSet};
+use std::collections::{VecDeque, HashSet, HashMap};
 
 const MAX_SIZE: usize = 15;
 
@@ -57,12 +57,13 @@ impl<const SIZE: usize> City<SIZE> {
 pub struct Links<const SIZE: usize> {
     city_a: City<SIZE>,
     city_b: City<SIZE>,
-    matrix: [[Option<usize>; SIZE]; 2]
+    matrix: [[Option<usize>; SIZE]; 2],
+    current_hash: u128
 }
 
 impl<const SIZE: usize> Links<SIZE> {
     pub fn new(city_a: City<SIZE>, city_b: City<SIZE>) -> Links<SIZE> {
-        Links { city_a, city_b, matrix: [[None; SIZE]; 2] }
+        Links { city_a, city_b, matrix: [[None; SIZE]; 2], current_hash: 0}
     }
 
     pub fn get_neighbor(&self, i: usize, city_b: bool) -> Option<usize> {
@@ -70,6 +71,11 @@ impl<const SIZE: usize> Links<SIZE> {
     }
 
     pub fn checked_add_link(&mut self, pa: usize, pb: usize) -> bool {
+    
+    
+        self.current_hash += ((pb + 1) << (pa * 4)) as u128;
+    
+    
         self.matrix[0][pa] = Some(pb);
         self.matrix[1][pb] = Some(pa);
 
@@ -85,16 +91,28 @@ impl<const SIZE: usize> Links<SIZE> {
     }
 
     pub fn remove_link(&mut self, pa: usize, pb: usize) {
+    
+        self.current_hash -= ((pb + 1) << (pa * 4)) as u128;
+
         self.matrix[0][pa] = None;
         self.matrix[1][pb] = None;
+    }
+    
+    pub fn get_current_hash(&self) -> u128 {
+        self.current_hash
     }
 
 }
 
 
 
-pub fn explore(links: &mut Links<MAX_SIZE>, not_linked_a: &mut VecDeque<usize>, not_linked_b: &mut VecDeque<usize>, number_of_calls: &mut usize) -> usize {
-    let mut current_max: usize = 0;
+pub fn explore(links: &mut Links<MAX_SIZE>, not_linked_a: &mut VecDeque<usize>, not_linked_b: &mut VecDeque<usize>, number_of_calls: &mut usize, memoization_table: &mut HashMap<u128, u8>) -> usize {
+    
+    if memoization_table.contains_key(&links.get_current_hash()) {
+        return *(memoization_table.get(&links.get_current_hash()).unwrap()) as usize
+    };
+    
+    let mut current_max: usize;
     let mut current_min: usize = MAX_SIZE + 42;
     let mut person_a: usize;
     let mut person_b: usize;
@@ -108,7 +126,7 @@ pub fn explore(links: &mut Links<MAX_SIZE>, not_linked_a: &mut VecDeque<usize>, 
             person_b = not_linked_b.pop_back().unwrap();
 
             if links.checked_add_link(person_a, person_b) {
-                current_max = usize::max(explore(links, not_linked_a, not_linked_b, number_of_calls) + 1, current_max);
+                current_max = usize::max(explore(links, not_linked_a, not_linked_b, number_of_calls, memoization_table) + 1, current_max);
             }
 
             not_linked_b.push_front(person_b);
@@ -121,27 +139,25 @@ pub fn explore(links: &mut Links<MAX_SIZE>, not_linked_a: &mut VecDeque<usize>, 
         current_min = usize::min(current_min, current_max);
 
 
-    } if current_min > MAX_SIZE {0} else {current_min}
+    };
+    
+    let output = if current_min > MAX_SIZE {0} else {current_min};
+    if memoization_table.len() < 200000000 {
+        memoization_table.insert(links.get_current_hash(), output.clone() as u8);
+    }
+    output
 }
-
-
-
-
-
-
-
-
 
 
 fn main() {
 
-    let mut city_a = City::new(8);
-    city_a.add_cycle(0, 2).add_cycle(2, 8);
+    let mut city_a = City::new(10);
+    city_a.add_cycle(0, 5).add_cycle(5, 10);
 
     city_a.precalculate_friends();
 
-    let mut city_b = City::new(8);
-    city_b.add_cycle(0, 8);
+    let mut city_b = City::new(10);
+    city_b.add_cycle(0, 10);
 
     city_b.precalculate_friends();
 
@@ -161,8 +177,9 @@ fn main() {
 
 
     let mut number_of_calls = 0_usize;
-
-    let max_capa = explore(&mut links, &mut not_linked_a, &mut not_linked_b, &mut number_of_calls);
+    let mut table: HashMap<u128, u8> = HashMap::new();
+    
+    let max_capa = explore(&mut links, &mut not_linked_a, &mut not_linked_b, &mut number_of_calls, &mut table);
     println!("Max capacity: {}, num: {}", max_capa, number_of_calls)
 
 
